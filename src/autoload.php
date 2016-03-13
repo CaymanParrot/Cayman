7e06c1ca4d1d38499fe476a0b347f051c70f2cc1
Haci Murat Yaman <haci@muratyaman.co.uk>
2016-03-13 15:27:38 +0000

    moved autoloader

--- src/autoload.php
+++ src/autoload.php
@@ -5,14 +5,12 @@
 
 spl_autoload_register(
     function($className) {
-        $dir = __DIR__;
         $ds = DIRECTORY_SEPARATOR;
         if (substr($className, 0, 6) == 'Cayman') {
-            $className = substr($className, 7);//drop 7 chars 'Cayman\'
             $className = str_replace('\\', $ds, $className);//  '\' ==> '/'
-            $file = $dir . $ds . $className . '.php';
+            $file = __DIR__ . $ds . $className . '.php';
             if (file_exists($file)) {
-                require_once $file;
+                require $file;
             }
         }
     }
